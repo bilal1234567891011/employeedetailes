6d91bd46e1c30795ba3a49ea902f16fda2a48336
unknown <bilalb.kabeer@gmail.com>
2023-05-18 12:04:23 +0530

    employeefrontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Admin from './components/Admin';
+import Add from './components/Add';
+import Edit from './components/Edit';
+import View from './components/View';
+import PagenotFound from './PagenotFound';
+import{Route,Routes} from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Navbar expand="lg" variant="light" bg="light">
+      <Container>
+        <Navbar.Brand href="#"><h1>Employee Managment App</h1> </Navbar.Brand>
+      </Container>
+    </Navbar>
+
+<Routes>
+
+<Route  path='/' element={<Admin></Admin>}></Route>
+<Route path='/add' element={<Add></Add>}></Route>
+<Route path='/edit/:id' element={<Edit/>}></Route>
+<Route path='/view/:id' element={<View/>}></Route>
+<Route path={'*'} element={<PagenotFound></PagenotFound>}></Route>
+
+
+
+</Routes>
+
     </div>
   );
 }
